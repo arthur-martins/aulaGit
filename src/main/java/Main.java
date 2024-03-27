2dbd2e9111d9d94f9e69e7cf013ef6ef99b9424d
arthur-martins <arthurm.dev@gmail.com>
2024-03-27 20:43:43 -0300

    Adiciona o JOptionPane

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,5 +1,7 @@
+import javax.swing.*;
+
 public class Main {
     public static void main(String[] args) {
-
+        JOptionPane.showMessageDialog(null, "Mensagem simples exibida para o usuário!");
     }
 }
